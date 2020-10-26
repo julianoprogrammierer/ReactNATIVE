f14186f6da6a345bd4de0af7410a813ab589abd3
julianoprogrammierer <julianoprogrammierer@gmail.com>
2020-10-26 10:27:33 -0300

    conected-api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
-import React from "react";
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StatusBar, StyleSheet } from 'react-native';
+
+import api from '../src/services/api';
 
 export default function App() {
-    return<View/>;
+    const [repositories, setRepositories] = useState([]);
+    
+    useEffect(() => {
+        api.get('repositories').then(response => {
+            setRepositories(response.data);
+        });
+    }, []);
+
+    return(
+    <>
+     <StatusBar barStyle="light-content"/>
+     <View style={styles.container}>
+    {repositories.map(repository => (
+    <Text style={styles.repository} key={repository.id}>{repository.title}</Text>))}
+
+     </View>
+     </>
+    );
 }
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#7159c1',
+        justifyContent:'center',
+        alignItems: "center"
+    },
+
+    repository: {
+        color:'#FFF',
+        fontSize: 30,
+        
+
+    },
+
+    
+});
